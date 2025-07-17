32d1c93835c9b3a56b0aca91670e38efc22a3351
Anjitha John William <98218883+Anjithajm@users.noreply.github.com>
2025-07-17 12:29:37 +0200

    Update model.py

--- model.py
+++ model.py
@@ -113,8 +113,10 @@ def main(args):
     x = Conv2D(64, (1, 1), padding='same', activation='relu', kernel_initializer='he_normal')(x)
     x = Conv2D(192, (3, 3), padding='same', activation='relu', kernel_initializer='he_normal')(x)
     x = AveragePooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
-    for _ in range(4):
-        x = inception_module(x, [64, 96, 128, 16, 32, 32])
+    x = inception_module(x, filters=[64, 96, 128, 16, 32, 32]) 
+    x = inception_module(x, filters=[64, 96, 128, 16, 32, 32]) 
+    x = inception_module(x, filters=[128, 128, 192, 32, 96, 64])
+    x = inception_module(x, filters=[128, 128, 192, 32, 96, 64])
     cnn_output = Flatten()(x)
 
     onn_input = Input(shape=(len(args.mag_columns),))
