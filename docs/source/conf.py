61211fd03c2329974d0f5674aa8a015d247aed85
Anjitha John William <98218883+Anjithajm@users.noreply.github.com>
2026-01-03 16:11:32 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,35 +1,55 @@
 # Configuration file for the Sphinx documentation builder.
 
-# -- Project information
+# -- Project information -----------------------------------------------------
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Hybrid-z'
+author = 'Anjitha M'
+copyright = '2026, Anjitha M'
 
-release = '0.1'
-version = '0.1.0'
+# The full version, including alpha/beta/rc tags
+release = '1.0'
+version = '1.0.0'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
 
+# Sphinx extensions
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    'sphinx.ext.duration',       # optional: build duration reporting
+    'sphinx.ext.doctest',        # optional: test snippets in docs
+    'sphinx.ext.autodoc',        # automatically document Python modules
+    'sphinx.ext.autosummary',    # generate API summary pages
+    'sphinx.ext.intersphinx',    # link to other projects docs
+    'sphinx.ext.napoleon',       # support NumPy / Google style docstrings
+    'sphinx_autodoc_typehints',  # optional: include type hints in docs
 ]
 
+# Intersphinx mapping (link to external documentation)
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    'tensorflow': ('https://www.tensorflow.org/api_docs/python', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
 }
 intersphinx_disabled_domains = ['std']
 
+# Paths for templates
 templates_path = ['_templates']
 
-# -- Options for HTML output
+# List of patterns to ignore
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# Generate autosummary pages automatically
+autosummary_generate = True
+
+# -- Options for HTML output -------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
-# -- Options for EPUB output
+# -- Options for EPUB output -------------------------------------------------
 epub_show_urls = 'footnote'
+
+# -- API module paths --------------------------------------------------------
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))  # Add repo root to Python path
